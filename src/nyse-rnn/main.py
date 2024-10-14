798e1a6d1751860f3bd0957ff2485f2d987c82bf
vibhorpundhir <112891527+vibhorpundhir@users.noreply.github.com>
2024-10-14 18:21:46 +0530

    Update main.py
    Try this one

--- src/nyse-rnn/main.py
+++ src/nyse-rnn/main.py
@@ -4,153 +4,50 @@ from nyse import *
 import nn
 import classification_performance as cp
 from plotting import *
+import pickle
+import numpy as np
 
-
-def fs():
-    input_length = 100
-    hidden_cnt = 50
-    data = get_test_data(input_length)
-
-    rnn_nn = nn.NeuralNetwork(nn=rnn.RNN(input_length, hidden_cnt, data.x.shape[2], data.y.shape[1]),
-                              validation_split=0.2,
-                              batch_size=256,
-                              nb_epoch=10,
-                              show_accuracy=True)
-
-    features, results = rnn_nn.feature_selection(data)
-    print("Selected features: {0}".format(features))
-    print(results)
-
-    feature_selection = {"features": features, "results": results, "count": data.x.shape[2]}
-    output = open('../../results/RNN_features', 'wb')
-    pickle.dump(feature_selection, output)
-    output.close()
-
-
-def rrn_iter_error(iters=200):
-
-    input_length = 100
-    hidden_cnt = 100
-    data = get_test_data(input_length)
-
-    print("input length ", input_length)
-    print("hidden_cnt ", hidden_cnt)
-
-    errors = {}
-    errors["test"] = []
-    errors["train"] = []
-
+# Helper function to split data into training and testing sets
+def split_data(data, split_ratio=0.5):
     n = data.x.shape[0]
+    split_idx = int(n * split_ratio)
+    return Data(data.x[:split_idx, :], data.y[:split_idx, :]), Data(data.x[split_idx:, :], data.y[split_idx:, :])
 
-    x_train = data.x[:(n/2), :]
-    y_train = data.y[:(n/2), :]
-    x_test = data.x[(n/2):, :]
-    y_test = data.y[(n/2):, :]
-
-    data_train = Data(x_train, y_train)
-    data_test = Data(x_test, y_test)
-
-    print('train x shape:', data_train.x.shape)
-    print('train y shape:', data_train.y.shape)
-    print('test x shape:', data_test.x.shape)
-    print('test y shape:', data_test.y.shape)
-
-    print("----------------------------------------------------------------------")
-    print("TRAIN RNN")
-    print("----------------------------------------------------------------------")
-
-    # try to get best nn
-    best_error = 1
-    for x in range(5):
-        rnn_nn = nn.NeuralNetwork(rnn.RNN(input_length, hidden_cnt, data.x.shape[2], data.y.shape[1]), nb_epoch=20)
-        error_train = rnn_nn.train(data_train)
+# Generalized function to train a neural network
+def train_neural_network(model, data_train, data_test, iters, epoch=10):
+    errors = {"train": [], "test": []}
+    best_error = float("inf")
+    
+    # Train multiple models and select the best one
+    for _ in range(5):
+        nn_model = nn.NeuralNetwork(model, nb_epoch=20)
+        error_train = nn_model.train(data_train)
         if error_train < best_error:
             best_error = error_train
-            best_nn = rnn_nn
+            best_nn = nn_model
 
-    rnn_nn = best_nn
-    rnn_nn.nb_epoch = 10
+    nn_model = best_nn
+    nn_model.nb_epoch = epoch
+    
     for i in range(iters):
-        print("-----------------------------")
-        print("ITERATION {0} FROM {1}".format(i, iters))
-        print("-----------------------------")
-
-        error_train = rnn_nn.train(data_train)
-        print("Train ERROR: {0}".format(error_train))
-        error_tst = rnn_nn.test(data_test)
-        print("Test ERROR: {0}".format(error_tst))
+        print(f"--- ITERATION {i+1} / {iters} ---")
+        error_train = nn_model.train(data_train)
+        error_test = nn_model.test(data_test)
+        
         errors["train"].append(error_train)
-        errors["test"].append(error_tst)
+        errors["test"].append(error_test)
 
-    print(errors)
+        print(f"Train ERROR: {error_train}")
+        print(f"Test ERROR: {error_test}")
 
-    output = open('../../results/RNN_errors', 'wb')
-    pickle.dump(errors, output)
-    output.close()
+    return errors
 
+# Generalized function to save results using a context manager
+def save_results(filename, data):
+    with open(filename, 'wb') as output:
+        pickle.dump(data, output)
 
-def mlp_iter_error(iters=500):
-
-    input_length = 50
-    hidden_cnt = 100
-    data = get_test_data(input_length)
-
-    print("input length ", input_length)
-    print("hidden_cnt ", hidden_cnt)
-
-    errors = {}
-    errors["test"] = []
-    errors["train"] = []
-
-    n = data.x.shape[0]
-
-    x_train = data.x[:(n/2), :]
-    y_train = data.y[:(n/2), :]
-    x_test = data.x[(n/2):, :]
-    y_test = data.y[(n/2):, :]
-
-    data_train = Data(x_train, y_train)
-    data_test = Data(x_test, y_test)
-
-    print('train x shape:', data_train.x.shape)
-    print('train y shape:', data_train.y.shape)
-    print('test x shape:', data_test.x.shape)
-    print('test y shape:', data_test.y.shape)
-
-    print("----------------------------------------------------------------------")
-    print("TRAIN MLP")
-    print("----------------------------------------------------------------------")
-
-    # try to get best nn
-    best_error = 1
-    for x in range(4):
-        rnn_nn = nn.NeuralNetwork(perceptron.MLP(input_length, hidden_cnt, data.x.shape[2], data.y.shape[1]), nb_epoch=20)
-        error_train = rnn_nn.train(data_train)
-        if error_train < best_error:
-            best_error = error_train
-            best_nn = rnn_nn
-
-    rnn_nn = best_nn
-    rnn_nn.nb_epoch = 10
-    for i in range(iters):
-        print("-----------------------------")
-        print("ITERATION {0} FROM {1}".format(i, iters))
-        print("-----------------------------")
-
-        error_train = rnn_nn.train(data_train)
-        print("Train ERROR: {0}".format(error_train))
-        error_tst = rnn_nn.test(data_test)
-        print("Test ERROR: {0}".format(error_tst))
-        errors["train"].append(error_train)
-        errors["test"].append(error_tst)
-
-    print(errors)
-
-    output = open('../../results/MLP_errors', 'wb')
-    pickle.dump(errors, output)
-    output.close()
-
-
+# Main function to train both RNN and MLP models
 def main():
     input_length = 25
     hidden_cnt = 100
@@ -158,47 +55,28 @@ def main():
     epochs = 200
     data = get_test_data(input_length)
     
-    print("----------------------------------------------------------------------")
-    print("TRAIN RNN")
-    print("----------------------------------------------------------------------")
-
-    rnn_nn = nn.NeuralNetwork(rnn.RNN(input_length, hidden_cnt, data.x.shape[2], data.y.shape[1]))
-    rnn_data = data
-    rnn_nn.nb_epoch = epochs
-    rnn_errors_train, rnn_errors_tst = rnn_nn.run_with_cross_validation(rnn_data, cross_validation_passes)
- 
-    print("----------------------------------------------------------------------")
-    print("RNN AVERAGE ERROR = {0}".format(np.average(rnn_errors_tst)))
-    print("RNN TEST ERRORS = {0}".format(rnn_errors_tst))
-    print("----------------------------------------------------------------------")
-    print()
-    print()
-    print("----------------------------------------------------------------------")
-    print("TRAIN MLP")
-    print("----------------------------------------------------------------------")
-
-    mlp_nn = nn.NeuralNetwork(perceptron.MLP(input_length, hidden_cnt, data.x.shape[2], data.y.shape[1]))
-    mlp_data = data
-    mlp_nn.nb_epoch = epochs
-    mlp_errors_train, mlp_errors_tst = mlp_nn.run_with_cross_validation(mlp_data, cross_validation_passes)
-
-    print("----------------------------------------------------------------------")
-    print("MLP AVERAGE ERROR = {0}".format(np.average(mlp_errors_tst)))
-    print("MLP TEST ERRORS = {0}".format(mlp_errors_tst))
-    print("----------------------------------------------------------------------")
-
+    # Split data into train/test sets
+    data_train, data_test = split_data(data)
+    
+    # Train and evaluate RNN
+    print("Training RNN...")
+    rnn_model = rnn.RNN(input_length, hidden_cnt, data.x.shape[2], data.y.shape[1])
+    rnn_errors = train_neural_network(rnn_model, data_train, data_test, cross_validation_passes, epochs)
+    
+    print("Training MLP...")
+    mlp_model = perceptron.MLP(input_length, hidden_cnt, data.x.shape[2], data.y.shape[1])
+    mlp_errors = train_neural_network(mlp_model, data_train, data_test, cross_validation_passes, epochs)
+    
+    # Display performance metrics
     perf = cp.ClassificationPerformance()
-    perf.add("RNN", rnn_errors_tst)
-    perf.add("MLP", mlp_errors_tst)
+    perf.add("RNN", rnn_errors["test"])
+    perf.add("MLP", mlp_errors["test"])
     perf.compare()
     perf.make_plots()
 
+    # Save results
+    save_results('../../results/RNN_errors', rnn_errors)
+    save_results('../../results/MLP_errors', mlp_errors)
 
 if __name__ == '__main__':
     main()
-    # fs()
-    # plot_features()
-    # rrn_iter_error()
-    # rrn_iter_error_plot()
-    # mlp_iter_error()
-    # mlp_iter_error_plot()
